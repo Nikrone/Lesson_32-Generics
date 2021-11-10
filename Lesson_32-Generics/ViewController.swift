2a016a00fc996e0bbfddbe63c35aa5536a36c435
Evgeniy Nosko <77491382+Nikrone@users.noreply.github.com>
2021-11-10 19:40:08 +0300

    added part 1 Lesson 32 - Generics

--- Lesson_32-Generics/ViewController.swift
+++ Lesson_32-Generics/ViewController.swift
@@ -8,12 +8,53 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+//        print(sum(firstParam: "123", secondParam: "123"))
+        
+        let sumClass = SumOfElements<String>(firstParam: "123", secondParam: "123")
+        print(sumClass.sum())
+
     }
-
-
+    
+//        func sum (firstParam: Int, secondParam: Int) -> Int {
+//            return firstParam + secondParam
+//        }
+    
+    //    общий тип - <T>
+//    func sum() -> T? {
+//        if let firstParam = firstParam as? Int, let secondParam = secondParam as? Int {
+//            return (firstParam + secondParam) as? T
+//        } else if let firstParam = firstParam as? String, let secondParam = secondParam as? String {
+//            return (firstParam + secondParam) as? T
+//        } else {
+//            return nil
+//        }
+//    }
+    
+   
+    
 }
 
+
+class SumOfElements<T> {
+    private var firstParam: T
+    private var secondParam: T
+    
+    init(firstParam: T, secondParam: T) {
+        self.firstParam = firstParam
+        self.secondParam = secondParam
+    }
+    
+    
+    func sum() -> T? {
+        if let firstParam = firstParam as? Int, let secondParam = secondParam as? Int {
+            return (firstParam + secondParam) as? T
+        } else if let firstParam = firstParam as? String, let secondParam = secondParam as? String {
+            return (firstParam + secondParam) as? T
+        } else {
+            return nil
+        }
+    }
+}
